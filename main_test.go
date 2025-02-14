0f769065ce94761e8642d20485834f91c60fb60f
maxim-ge <maxim.ge@gmail.com>
2025-02-14 14:46:30 +0100

    Add unit tests for main command execution in main_test.go

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_MainDraft(t *testing.T) {
+	require := require.New(t)
+
+	err := execRootCmd([]string{"reqmd", "trace", "C:/workspaces/work/voedger-internals"}, "0.0.1")
+	require.Nil(err)
+
+	require.NotNil(t)
+}
