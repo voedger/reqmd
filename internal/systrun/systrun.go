eaf77d08b584256e1d1d6dc6c4255f12f24394ff
maxim-ge <m.geraskin@untill.com>
2025-04-12 20:08:55 +0200

    Refactor system tests by replacing references to the obsolete 'systest' package with 'systrun', and remove outdated test files to streamline the testing framework.

--- internal/systrun/systrun.go
+++ internal/systrun/systrun.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2025-present unTill Software Development Group B. V. and Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-package systest
+package systrun
 
 import (
 	"bytes"
